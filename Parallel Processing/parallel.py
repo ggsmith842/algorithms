197abc36179adb95fd29ebbf2ba95b2d2ed3abed
ggsmith842 <grant.oferrell.smith@gmail.com>
2024-08-18 12:00:12 -0600

    add multicore example

---
+++ Parallel Processing/parallel.py
@@ -0,0 +1,28 @@
+from concurrent.futures import ProcessPoolExecutor, as_completed
+from time import sleep, time
+
+
+# define a cpu intensive process as those are the most beneficial from multicore processing vs multithread.
+def cpu_intensive_process(taskid: int):
+    sleep(5) # simulate waiting for a computaion to complete
+    return f"task with id {taskid} complete"
+
+if __name__ == '__main__':
+
+    tasks = [1, 2, 3, 4]
+
+    
+    # parallel process using multiple cores
+    start = time()
+    with ProcessPoolExecutor() as executor:
+
+        for task, status in zip(tasks, executor.map(cpu_intensive_process, tasks)):
+            print(task, status)
+
+    end = time() - start
+    print(f'Total Execution time: {end}s')
+
+
+
+
+
